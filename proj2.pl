2368ba9a9349c3b3d08015159c7468f8573c4e05
omjadas <omja.das@gmail.com>
2019-10-15 16:39:07 +1100

    added scaffolding

--- proj2.pl
+++ proj2.pl
@@ -1,1 +1,19 @@
-puzzle_solution(Solution).
+puzzle_solution(Solution) :-
+    diagonal(Solution),
+    digits(Solution),
+    repeats(Solution),
+    rows(Solution).
+
+diagonal(Solution).
+
+digits(Solution).
+
+repeats(Solution).
+
+rows(Solution).
+
+valid(Solution):- product(Solution); sum(Solution).
+
+product(Solution).
+
+sum(Solution).
